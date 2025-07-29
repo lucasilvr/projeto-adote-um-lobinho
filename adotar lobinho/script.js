e879311940845501644c6d1050609db62a593ea2
Lucas <lucassilveira07.ls@gmail.com>
2025-07-29 18:29:41 -0300

    fix: correçoes finais

---
+++ adotar lobinho/script.js
@@ -0,0 +1,50 @@
+let todosOsLobos = JSON.parse(localStorage.getItem("lobos")) || [];
+const urlParams = new URLSearchParams(window.location.search);
+const loboId = urlParams.get("id");
+const lobo = todosOsLobos.find((l) => l.id == loboId); //isso daqui coleta o id
+
+const infoContainer = document.querySelector(".primeiro");
+
+if (lobo) {
+  const loboHtml = `
+            <img id="imagem-lobo" src="${lobo.imagem} class="imagem">
+            <div class="adote">
+                <h1 id="titulo-adocao">Adote o(a) ${lobo.nome}</h1>
+                <p id="id-lobo">ID: ${lobo.id}</p>
+            </div>
+        `;
+  infoContainer.innerHTML = loboHtml;
+}
+const form = document.querySelector(".segundo");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+  if (!lobo) {
+    window.location.href ="../lista-de-lobinhos/index.html";
+    return;
+  }
+
+  let nomeDono = document.getElementById("nome").value;
+  let idadeDono = document.getElementById("idade").value;
+  let emailDono = document.getElementById("email").value;
+
+  if (!nomeDono || !idadeDono || !emailDono) {
+    alert("Por favor, preencha todos os seus dados para adotar!");
+    return;
+  }
+
+//isso aqui coleta o id
+let id = todosOsLobos.findIndex((l) => l.id == loboId);
+
+  todosOsLobos[id].adotado = true;
+  todosOsLobos[id].nomeDono = nomeDono;
+  todosOsLobos[id].idadeDono = parseInt(idadeDono);
+  todosOsLobos[id].emailDono = emailDono;
+
+  localStorage.setItem("lobos", JSON.stringify(todosOsLobos));
+
+  alert(` Você adotou o(a) ${lobo.nome}!`);
+  window.location.href = "../lista-de-lobinhos/index.html";
+
+});
--- show/script.js
+++ show/script.js
@@ -1,5 +1,4 @@
 const todosOsLobos = JSON.parse(localStorage.getItem("lobos"));
-
 // isso aqui pega o id do lobo da url da parte "?id=5" pega o número 5, pesquisei sobre
 const idDoLobo = window.location.search.split("=")[1];
 
@@ -17,14 +16,27 @@ if (loboEncontrado) {
   document.getElementById("imagem-lobo").src = loboEncontrado.imagem;
   document.getElementById("descricao").textContent = loboEncontrado.descricao;
 }
-//so fiz o de excluir no script pq o do de adotar da pra fazer no html
+
+const botaoAdotar = document.querySelector(".btn-adotar");
 const botaoExcluir = document.querySelector(".btn-excluir");
+
+//aqui se o lobo estiver adotado, ele desativa o botao
+if (loboEncontrado.adotado == true) {
+  botaoAdotar.disabled = true;
+  botaoAdotar.style.cursor = 'not-allowed';
+}
+
+if (botaoAdotar){
+    botaoAdotar.addEventListener("click", () => {
+      window.location.href = `../adotar lobinho/adotar.html?id=${idDoLobo}`;
+    })
+}
 
 if (botaoExcluir) {
   botaoExcluir.addEventListener("click", () => {
     const novaListaDeLobos = todosOsLobos.filter((l) => l.id != idDoLobo);
     localStorage.setItem("lobos", JSON.stringify(novaListaDeLobos));
     alert("Lobinho excluído");
-    window.location.href = "../lista-de-lobinhos/index.html";
+    window.location.href = `../lista-de-lobinhos/index.html`;
   });
 }
